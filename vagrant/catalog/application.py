822338bbe8d8f142464834c87d0ee45178dad838
Emmanouil Kardaris <emmanouilkardaris@gmail.com>
2017-08-19 03:58:14 +0300

    Update application.py

---
+++ vagrant/catalog/application.py
@@ -0,0 +1,33 @@
+from functools import wraps
+
+from flask import Flask, render_template, request, redirect, jsonify, flash
+from flask import session as login_session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from database_setup import Base, Category, Items
+
+import random
+import string
+
+# IMPORTS FOR THIS STEP
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = "super secret key"
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalog Application"
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
